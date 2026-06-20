12b8b2d6ae07755fc23298b7bb3599fafb5a7376
jeveraerdt <jens.everaerdt@groenhoveschool.be>
2026-06-20 15:59:36 +0200

    Update endpoint URL in voortgang-config.js

--- 1ejaar/latijn/oefeningen/grammatica/voortgang-config.js
+++ 1ejaar/latijn/oefeningen/grammatica/voortgang-config.js
@@ -6,7 +6,7 @@
   alleen PASTE_NEW_1EJAAR_EXEC_URL door de nieuwe URL die op /exec eindigt.
 */
 window.LATIJN_VOORTGANG_SYNC = {
-  endpoint: 'PASTE_NEW_1EJAAR_EXEC_URL',
+  endpoint: 'https://script.google.com/macros/s/AKfycbw5G-y6m0CfVVg1rs0sE87qeDLir8rEJRHz6ntoW7yi9VL3Q9K_nkBYPOpvLUA5kNI/exec',
   enabled: true,
   classId: '1GR-LAT',
   school: 'athena campus Pottelberg',
