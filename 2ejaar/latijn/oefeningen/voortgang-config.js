7cd525402067851fcc431bf76d94d4903a4145cb
jeveraerdt <jens.everaerdt@groenhoveschool.be>
2026-06-20 11:47:38 +0200

    Add files via upload

---
+++ 2ejaar/latijn/oefeningen/voortgang-config.js
@@ -0,0 +1,11 @@
+/*
+  Plaats dit bestand in dezelfde map als oefeningen-vervoegingen.html.
+  Vervang enkel de URL tussen de aanhalingstekens na endpoint.
+  Laat endpoint leeg zolang de Google Apps Script-webapp nog niet is gedeployed.
+*/
+window.LATIJN_VOORTGANG_SYNC = {
+  endpoint: 'https://script.google.com/macros/s/AKfycbzG66w4Ld5XWN-zpDLRXesbM7YWfhqceaJBq6_F8EEGf4nwSbK0P9ur7TCphPVaL8w/exec',
+  classId: '2GR-LAT',
+  school: 'athena campus Pottelberg',
+  enabled: true
+};
