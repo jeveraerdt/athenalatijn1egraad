20429fe529c01f31cde7d1e50c7d2d4c2d17c336
jeveraerdt <jens.everaerdt@groenhoveschool.be>
2026-06-20 13:06:25 +0200

    Remove duplicate 'enabled' property in config

--- 2ejaar/latijn/oefeningen/voortgang-config.js
+++ 2ejaar/latijn/oefeningen/voortgang-config.js
@@ -1,10 +1,7 @@
 window.LATIJN_VOORTGANG_SYNC = {
   endpoint: 'https://script.google.com/macros/s/AKfycbzG66w4Ld5XWN-zpDLRXesbM7YWfhqceaJBq6_F8EEGf4nwSbK0P9ur7TCphPVaL8w/exec',
-
+  enabled: true,
   classId: '2GR-LAT',
   school: 'athena campus Pottelberg',
-
-  enabled: true,
-
   grammarDashboardVersion: 2
 };
